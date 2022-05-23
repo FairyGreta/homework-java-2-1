05e25069e9fba1ee6efaec7a0f08e364c83c7295
Kristina Artyushechkina <104067981+FairyGreta@users.noreply.github.com>
2022-05-23 12:45:25 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int ticket = 8_542; //стоимость билета
-        int x = 20; // количество рублей для одной бонусной мили
+        int bonus = 20; // количество рублей для одной бонусной мили
 
         int bonusMiles = ticket / x;
         System.out.println("Начислено миль: " + bonusMiles);
